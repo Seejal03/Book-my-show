431ed0ca776638210494b7df3d7bc806c7dd9d88
Seejal Sood <seejalsood03@gmail.com>
2025-03-28 01:35:46 +0530

    ui changed for the movies tab

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import SelectMovie from '../Components/SelectMovie'
+import LastBooking from '../Components/LastBooking'
+import Timings from '../Components/Timmimgs'
+import SelectSeat from '../Components/SelectSeat'
+import "../CSS/home.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className='container'>
+      <div className='wrap'>
         <div className='select_movie_component'>
         <SelectMovie/>
         </div>
+        <div className='Last_booking'>
+          <LastBooking/>
+        </div>
     </div>
+    <div className='seat_booking'>
+      <Timings/>
+      <SelectSeat/>
+      <button className='submit'> Book Now</button>
+    </div>
+    </div>
+    
   )
 }
 
