036a8adbf2e21194110c0d3cbb81df113420dc28
Seejal Sood <seejalsood03@gmail.com>
2025-03-28 02:00:45 +0530

    timing slots added

--- front-end/src/Components/Timmimgs.js
+++ front-end/src/Components/Timmimgs.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Radio from './Radio'
+import "../CSS/selectslots.css"
+import { slots } from '../data'
 const Timings = () => {
   return (
-    <div>SeatBooking</div>
+    <>
+    <div className='Slots_conatiner'>
+      <h1 className='Slots_heading'>Select a slot</h1>
+      <div className='slots'>
+        {slots.map((element, index)=>{
+        return (
+          <Radio   text={element} />
+        )
+      })}</div>
+    </div>
+    </>
   )
 }
 
