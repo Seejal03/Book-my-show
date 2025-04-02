a0fd28173a0a60482efb3e6dfccec7288bebcf67
Seejal Sood <seejalsood03@gmail.com>
2025-04-02 23:10:49 +0530

    last booking details ui correted

--- front-end/src/Components/LastBooking.js
+++ front-end/src/Components/LastBooking.js
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import { seats } from '../data'
+import '../CSS/lastbooking.css'
 const LastBooking = () => {
   return (
-    <div>LastBooking</div>
+    <div className='main'>
+    <h1 className='main-heading'> Last Booking</h1>
+    <div className='seats-container'>
+      <p className='seats-header'>Seats</p>
+      <ul className='seats-list'>
+      {seats.map((seat, index) => ( 
+  <li className='seats_val' key={index}>
+    {seat}:0
+  </li>
+))}
+
+
+      </ul>
+    </div>
+    <p className="slot" style={{ textAlign: "left" }}>
+            Slot: <span>00</span>
+          </p>
+          <p className="movie">
+            Movie: <span>ABC</span>
+          </p>      
+    </div>
   )
 }
 
